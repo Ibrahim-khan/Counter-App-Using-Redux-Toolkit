71ba905c404df2410ded1f3628c1927b9dc0bdf7
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-06-10 17:29:03 +0600

    Counter-App-Using-Redux-Toolkit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import CounterView from "./counter/CounterView";
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Redux Toolkit</h1>
+        <CounterView />
+      </div>
+    </>
+  )
+}
+
+export default App
